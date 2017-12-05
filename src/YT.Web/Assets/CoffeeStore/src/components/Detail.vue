e9b1f7d5c3001b0c7bbbe1b73e211dea242088d1
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-12-05 10:33:14 +0800

    www

--- src/YT.Web/Assets/CoffeeStore/src/components/Detail.vue
+++ src/YT.Web/Assets/CoffeeStore/src/components/Detail.vue
@@ -5,7 +5,7 @@
     <group>
       <x-number title="浓度" :min="0" :max="5" v-model="call.o"></x-number>
       <x-number title="奶度" :min="0" :max="5" v-model="call.m"></x-number>
-      <x-number title="糖度" :min="0" :max="5" v-model="call.t"></x-number>
+      <x-number title="糖度" :min="0" :max="5" v-model="call.s"></x-number>
       <selector title="选择代金券" :options="cards" v-model="selectCard"></selector>
       <cell title="总金额" v-model="model.price"></cell>
     </group>
@@ -69,6 +69,9 @@
           m: 0,
           s: 0
         },
+        order:"",
+        type:null,
+        fastcode:"",
         selectCard: null,
         model: {},
         cards: [],
@@ -77,18 +80,17 @@
     },
     methods: {
       init() {
-        debugger;
         const params = this.$route.params.id.split("-");
-        const fastcode=params[2].split("");
-        this.call.o=fastcode[0];
-        this.call.m=fastcode[1];
-        this.call.s=fastcode[2];
+        const fastcode = params[2].split("");
+        this.call.o = fastcode[0];
+        this.call.m = fastcode[1];
+        this.call.s = fastcode[2];
         // pid-did-fastcode-order-type
         // 102-10152-222-2017120509563310152abcde-1
         const url =
           "http://103.45.102.47:8888/api/services/app/mobile/GetProduct";
         this.$http.post(url, {
-          id: this.$route.params.id
+          id: params[0]
         }).then(r => {
           if (r.data && r.data.result) {
             const element = r.data.result;
