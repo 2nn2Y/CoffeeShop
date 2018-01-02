b5aafdd9df31c2763fcbd972ab8a67c1efe5e936
zhaohejing2017 <zhaohejing@zhj125.cn>
2018-01-02 13:37:57 +0800

    wwww

--- src/YT.Web/App_Start/YtWebModule.cs
+++ src/YT.Web/App_Start/YtWebModule.cs
@@ -98,7 +98,7 @@ namespace YT.Web
             var controller = IocManager.Resolve<SignController>();
             //同步订单  每12分钟一次
             RecurringJob.AddOrUpdate(() => background.GenderOrder(), "0/12 * * * *");
-            RecurringJob.AddOrUpdate(() => background.GenderTodayOrder(), "0 0/6 * * *");
+            RecurringJob.AddOrUpdate(() => background.GenderTodayOrder(), "0 6,12,18,23 * * *");
             // RecurringJob.AddOrUpdate(() => background.GenderMonthOrder(), "0/59 * * * *");
             //  同步报警信息  15分钟一次
             RecurringJob.AddOrUpdate(() => controller.GenderWarning(), "0/15 * * * *");
